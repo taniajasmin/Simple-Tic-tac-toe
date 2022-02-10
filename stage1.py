c479f0fe8cc056a1bbf9b3f7c8b9e7dbd42ec569
taniajasmin <99424140+taniajasmin@users.noreply.github.com>
2022-02-11 00:20:15 +0600

    Update stage1.py

--- stage1.py
+++ stage1.py
@@ -1,16 +1,3 @@
-data = input()
-print("---------")
-print("|", data[0],data[1],data[2],"|")
-print("|", data[3],data[4],data[5],"|")
-print("|", data[6],data[7],data[8],"|")
-print("---------")
-
-'''
-cells = input("Enter cells: ")
-DASHES = "-" * 9
-print(DASHES)
-for row in range(3):
-	rowstr = list(cells[3 * row : 3 * row + 3])
-	print(f'| {" ".join(rowstr)} |')
-print(DASHES)
-'''
+print("""X O X
+O X O
+X X O""")
